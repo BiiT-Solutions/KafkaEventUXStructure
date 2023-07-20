37fd342eda489f5f44e8ec589f99a733dd8da4e3
Jesus Pastor <jnavalon@biit-solutions.com>
2023-07-20 13:02:45 +0200

    moved angular version to v15-lts

--- projects/kafka-event-structure-lib/src/lib/services/auth.service.ts
+++ projects/kafka-event-structure-lib/src/lib/services/auth.service.ts
@@ -1,53 +1,54 @@
 import { Injectable } from '@angular/core';
 import {RootService} from "./root.service";
 import {HttpClient, HttpResponse} from "@angular/common/http";
-import {Event} from "../models/event";
 import {Observable} from "rxjs";
 import {AuthorizedUser} from "../models/authorized-user";
+import {
+  AuthService as AuthenticationService,
+  AuthCalls,
+  CreateUserRequest,
+  LoginRequest,
+  PasswordRequest,
+  User,
+  TokenRenewListener
+} from "authorization-services-lib";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-
-  private static readonly ROOT_PATH: string = '/auth';
-  private static readonly FIRST_TIMER_TIME: number = 1000;
-  private interval: number;
-  constructor(private rootService: RootService, private httpClient: HttpClient) { }
-  public autoRenewToken(token: string, callback: (token: string) => void): void {
-    if (this.interval != null) {
-      clearInterval(this.interval);
-      this.interval = null;
-    }
-    this.setIntervalRenew(token, AuthService.FIRST_TIMER_TIME, callback);
-
+export class AuthService implements AuthCalls{
+  add(user: User): Observable<User> {
+    return undefined;
   }
 
-  private setIntervalRenew(token: string, timeout: number, callback: (token: string) => void): void {
-    this.interval = setInterval((): void => {
-      this.renew(token).subscribe(
-        (res: HttpResponse<AuthorizedUser>) => {
-          const authToken: string = res.headers.get('authorization');
-          let expiration: number = Number(res.headers.get('expires'));
-          if (!authToken || !expiration) {
-            throw new Error('Server returned invalid response');
-          }
-          if (isNaN(expiration)) {
-            throw new Error('Server returned invalid expiration time');
-          }
-          expiration = expiration - (new Date()).getTime();
-          console.log(`Next token expiration: ${expiration}`);
-          callback(authToken);
-          this.setIntervalRenew(authToken, expiration, callback);
-        }
-      )
-    }, timeout)
+  autoRenewToken(token: string, expiration: number, tokenRenewListener: TokenRenewListener, tolerance: number): void {
   }
-  public renew(token ?: string): Observable<HttpResponse<AuthorizedUser>> {
-    return token ?
-      this.httpClient.get<HttpResponse<AuthorizedUser>>(
-        `${this.rootService.serverUrl}${AuthService.ROOT_PATH}/jwt/renew`, {headers: {'Authorization': 'Bearer ' + token}})
-      : this.httpClient.get<HttpResponse<AuthorizedUser>>(
-      `${this.rootService.serverUrl}${AuthService.ROOT_PATH}/jwt/renew`);
+
+  cancelAutoRenew(): void {
   }
+
+  changePassword(request: PasswordRequest): Observable<void> {
+    return undefined;
+  }
+
+  deleteByUserName(username: string): Observable<void> {
+    return undefined;
+  }
+
+  getAll(): Observable<User[]> {
+    return undefined;
+  }
+
+  getRoles(): Observable<string[]> {
+    return undefined;
+  }
+
+  login(request: LoginRequest): Observable<HttpResponse<User>> {
+    return undefined;
+  }
+
+  update(request: CreateUserRequest): Observable<User> {
+    return undefined;
+  }
+
 }
