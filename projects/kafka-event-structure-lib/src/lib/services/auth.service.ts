9ebba7d35e440d20a8e1766f84dc3f90803e6ba8
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-11-04 16:48:54 +0100

    Releasing as open source.

--- projects/kafka-event-structure-lib/src/lib/services/auth.service.ts
+++ projects/kafka-event-structure-lib/src/lib/services/auth.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {AuthService as AuthenticationService} from "authorization-services-lib";
+import {AuthService as AuthenticationService} from "@biit-solutions/authorization-services";
 import {KafkaEventStructureRootService} from "./kafka-event-structure-root.service";
 
 @Injectable({
